d538a1dd9440f406f13a53782995113091bc9977
Miguel Martin <miguel.angel.mdos120@gmail.com>
2025-05-30 13:46:54 +0000

    I see this error with the app, reported by NextJS, please fix it. The error is reported as HTML but presented visually to the user).
    A > before the line number in the error source usually indicates the line of interest:
    
    > Unhandled Runtime Error: Error: Event handlers cannot be passed to Client Component props.
    >   <button className=... ref=... onClick={function handleExportData} children=...>
    >                                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^
    > If you need interactivity, consider converting part of this to a Client Component.. Error source: Call Stack
    > 4
    >
    > Show 4 ignore-listed frame(s)

--- src/app/(main)/settings/page.tsx
+++ src/app/(main)/settings/page.tsx
@@ -1,3 +1,6 @@
+
+"use client"; // Added this directive
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
